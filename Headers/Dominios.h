87d3f909038fa75b55ebba6659a8815e4083000e
Daniel Campos <camposilvadaniel@gmail.com>
2024-03-29 15:55:56 -0300

    adiciona a classe  'codigo de pagamento' e suas funcionalidades

--- Headers/Dominios.h
+++ Headers/Dominios.h
@@ -35,3 +35,30 @@ public:
 };
 
 #endif
+
+/**
+* @brief Class representing the payment code (código de pagamento)
+*/
+class Codigo_de_pagamento{
+private:
+    std::string paymentCode;
+    void validatepaymentCode;
+public:
+    /**
+     * @brief Constructor for the payment Code.
+     * @param paymetncode: number string.
+     */
+     Codigo_de_pagamento(std::string paycode)
+     /**
+     * @brief Setter for the payment code number.
+     * @param payment code number string.
+     * @throw Throws an exception if the format is invalid.
+     */
+     void setPaymentcode(std::string paycode)
+     /**
+     * @brief Getter for the payment code number.
+     * @return The payment code number string.
+     */
+     std::string getPaycode();
+};
+#endif
--- Sources/Dominios.cpp
+++ Sources/Dominios.cpp
@@ -33,3 +33,36 @@ void CPF::setCPF(std::string cpf){
 std::string CPF::getCPF(){
     return CadastroPF;
 }
+
+
+
+// Codigo de pagamento constructor
+Codigo_de_pagamento::Codigo_de_pagamento(std::string pc_number){
+    paymentCode = pc_number;
+};
+//Validation method
+Codigo_de_pagamento::validatepaymentCode(std::string pc_number){
+    std::regex pattern("^[1-9]\\d{8} \\d{2}$")
+    std::string digitsOnly = pc_number;
+    digitsOnly.erase(std::remove(digitsOnly.begin(), digitsOnly.end(), ' '), digitsOnly.end());
+
+    for(auto x : digitsOnly){
+        if(!isdigit(x))
+            throw std::invalid_argument("Formato inválido. Apenas números são aceitos");
+    };
+    if (!std::regex_match(cpf, pattern)) {
+        throw std::invalid_argument("Formato inválido. Certifique-se de que o código de pagamento tenha o seguinte formato: 'XXXXXXXX XX'");
+    };
+};
+//Seter for the payment code
+void setPaymentcode(std::string paycode){
+    //Validate the format
+    validatepaymentCode(paycode);
+    paymentCode = paycode;
+
+};
+
+//Getter for the payment code
+std::string Codigo_de_pagamento::getPaycode(){
+    return paymentCode
+}
