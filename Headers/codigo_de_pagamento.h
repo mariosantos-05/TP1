21ba2a5eb903aa9a48e8bda11e29104d2b113171
Mario Santos <marioaugustov.s@gmail.com>
2024-03-30 14:46:18 -0300

    Adding the vscode folder to gitignore

--- Headers/codigo_de_pagamento.h
+++ Headers/codigo_de_pagamento.h
@@ -1,29 +1,31 @@
+#ifndef CODIGO_DE_PAGAMENTO_H
+#define CODIGO_DE_PAGAMENTO_H
 #include <string>     /**< For string manipulation. */
 #include <regex>      /**< For regular expressions. */
 
 /**
-* @brief Class representing the payment code (código de pagamento)
+* @brief Class representing the payment code (cï¿½digo de pagamento)
 */
 class Codigo_de_pagamento{
 private:
     std::string paymentCode;
-    void validatepaymentCode;
+    void validatepaymentCode(std::string pc_number);
 public:
     /**
      * @brief Constructor for the payment Code.
      * @param paymetncode: number string.
      */
-     Codigo_de_pagamento(std::string paycode)
-     /**
+    Codigo_de_pagamento(std::string paycode);
+    /**
      * @brief Setter for the payment code number.
      * @param payment code number string.
      * @throw Throws an exception if the format is invalid.
      */
-     void setPaymentcode(std::string paycode)
-     /**
+    void setPaymentcode(std::string paycode);
+    /**
      * @brief Getter for the payment code number.
      * @return The payment code number string.
      */
-     std::string getPaycode();
+    std::string getPaycode();
 };
 #endif
--- Sources/codigo_de_pagamento.cpp
+++ Sources/codigo_de_pagamento.cpp
@@ -1,4 +1,4 @@
-#include "codigo_de_pagamento.h" //<header file that give us the interface of codigo de pagamento>
+#include "Headers/codigo_de_pagamento.h" //<header file that give us the interface of codigo de pagamento>
 #include <stdexcept>
 
 
@@ -6,30 +6,30 @@
 Codigo_de_pagamento::Codigo_de_pagamento(std::string pc_number){
     paymentCode = pc_number;
 };
+
 //Validation method
-Codigo_de_pagamento::validatepaymentCode(std::string pc_number){
-    std::regex pattern("^[1-9]\\d{8} \\d{2}$")
+void Codigo_de_pagamento::validatepaymentCode(std::string pc_number){
+    std::regex pattern("^[1-9]\\d{8} \\d{2}$");
     std::string digitsOnly = pc_number;
     digitsOnly.erase(std::remove(digitsOnly.begin(), digitsOnly.end(), ' '), digitsOnly.end());
 
     for(auto x : digitsOnly){
         if(!isdigit(x))
-            throw std::invalid_argument("Formato inv·lido. Apenas n˙meros s„o aceitos");
+            throw std::invalid_argument("Formato inv√°lido. Apenas n√∫meros s√£o aceitos");
     };
-    if (!std::regex_match(cpf, pattern)) {
-        throw std::invalid_argument("Formato inv·lido. Certifique-se de que o cÛdigo de pagamento tenha o seguinte formato: 'XXXXXXXX XX'");
+    if (!std::regex_match(pc_number, pattern)) {
+        throw std::invalid_argument("Formato inv√°lido. Certifique-se de que o codigo de pagamento tenha o seguinte formato: 'XXXXXXXX XX'");
     };
 };
 //Seter for the payment code
-void setPaymentcode(std::string paycode){
+void Codigo_de_pagamento::setPaymentcode(std::string paycode){
     //Validate the format
     validatepaymentCode(paycode);
     paymentCode = paycode;
-
 };
 
 //Getter for the payment code
 std::string Codigo_de_pagamento::getPaycode(){
-    return paymentCode
-}
+    return paymentCode;
+};
 
