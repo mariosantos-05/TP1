86cbafd04d735d179e0490d3e9964a5ae618332e
himeakLucas <lucasadeodato.oli@gmail.com>
2024-04-09 10:53:30 -0300

    feat: Add class valor

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
-#include "Dominios.h"
+#include "../Headers/valor.h"
 
 //For now this file only gonna be used for us to test our classes
+
+int main() {
+    Valor dinheiro = Valor(10);
+    std::cout << dinheiro.getValor() << std::endl;
+    return 0;
+    
+}
---
+++ Sources/valor.cpp
@@ -0,0 +1,24 @@
+#include "../Headers/valor.h"
+
+
+bool Valor::validateValor(float valor)
+{
+    if (valor < 0 && valor > 1000000) {return false;}
+    return true;
+}
+
+Valor::Valor(float valor) {
+    this->valor = valor;
+}
+
+float Valor::getValor() {
+    return valor;
+}
+
+void Valor::setValor(float valor){
+    if(validateValor(valor)){
+        this->valor = valor;
+    }
+    else{ std::cout << "Valor Inválido" << std::endl;}
+}
+
