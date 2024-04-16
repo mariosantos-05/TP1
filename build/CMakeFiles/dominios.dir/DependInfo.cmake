5f09b415894c6218923a580c748f2a4dbb177d14
himeakLucas <lucasadeodato.oli@gmail.com>
2024-04-16 18:42:14 -0300

    fix: date validation is now working

--- build/CMakeFiles/dominios.dir/DependInfo.cmake
+++ build/CMakeFiles/dominios.dir/DependInfo.cmake
@@ -8,18 +8,18 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/mario/projetos/TP1/Sources/CPF.cpp" "CMakeFiles/dominios.dir/Sources/CPF.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/CPF.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Codigo_de_pagamento.cpp" "CMakeFiles/dominios.dir/Sources/Codigo_de_pagamento.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Codigo_de_pagamento.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Dinheiro.cpp" "CMakeFiles/dominios.dir/Sources/Dinheiro.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Dinheiro.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Estado.cpp" "CMakeFiles/dominios.dir/Sources/Estado.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Estado.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Percentual.cpp" "CMakeFiles/dominios.dir/Sources/Percentual.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Percentual.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Senha.cpp" "CMakeFiles/dominios.dir/Sources/Senha.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Senha.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/Setor.cpp" "CMakeFiles/dominios.dir/Sources/Setor.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Setor.cpp.o.d"
-  "/home/mario/projetos/TP1/Sources/main.cpp" "CMakeFiles/dominios.dir/Sources/main.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/main.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/CPF.cpp" "CMakeFiles/dominios.dir/Sources/CPF.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/CPF.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Data.cpp" "CMakeFiles/dominios.dir/Sources/Data.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Data.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Dinheiro.cpp" "CMakeFiles/dominios.dir/Sources/Dinheiro.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Dinheiro.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Estado.cpp" "CMakeFiles/dominios.dir/Sources/Estado.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Estado.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Percentual.cpp" "CMakeFiles/dominios.dir/Sources/Percentual.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Percentual.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Senha.cpp" "CMakeFiles/dominios.dir/Sources/Senha.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Senha.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/Setor.cpp" "CMakeFiles/dominios.dir/Sources/Setor.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/Setor.cpp.o.d"
+  "/home/lucas/dev/TP1/Sources/main.cpp" "CMakeFiles/dominios.dir/Sources/main.cpp.o" "gcc" "CMakeFiles/dominios.dir/Sources/main.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
