b751eafd20514975f1ad6587299c7a7de0bea385
himeakLucas <lucasadeodato.oli@gmail.com>
2024-04-17 08:17:16 -0300

    fix: remove prints to the console for debbuging in date class

--- Sources/Data.cpp
+++ Sources/Data.cpp
@@ -1,4 +1,4 @@
-#include "../Headers/Data.h"
+#include "Data.h"
 
 bool Data::is_leap_year(int year) {
 
@@ -16,8 +16,8 @@ int Data::month_lenth(int month, int year) {
         else {return 28;}
     }
 
-    if (month == 4 || month == 6 || month == 9 || month == 11) {return 31;} 
-    else {return 30;}
+    if (month == 4 || month == 6 || month == 9 || month == 11) {return 30;} 
+    else {return 31;}
 }
 
 bool Data::validadeHyphenPlacement(std::string data) {
@@ -37,11 +37,11 @@ bool Data::validateDateRanges(std::string data) {
 
     int month_lenth = this-> month_lenth(month, year);
 
-    if (year < 2000 && year > 2100) {return false;}
+    if (year < 2000 || year > 2100) {return false;}
     
-    if (month < 1 && month > 12) {return false;}
+    if (month < 1 || month > 12) {return false;}
 
-    if (day < 1 && day > month_lenth) {return false;}
+    if (day < 1 || day > month_lenth) {return false;}
 
     return true;
 }
@@ -57,6 +57,7 @@ bool Data::validate(std::string data) {
 
 void Data::set(std::string data) {
     if (validate(data)) {this->data = data;}
+    else {this->data = "data invalida";}
 }
 
 std::string Data::get() {return data;}
