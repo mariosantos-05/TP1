ef289d2884beaf072e02b97edb7eb5ce0ad3a877
Atila <atila108305@gmail.com>
2024-05-28 11:20:38 -0300

    Adicao de metodos e atributos faltantes na entidade Pagamento

--- Entities/Headers/Pagamento.h
+++ Entities/Headers/Pagamento.h
@@ -2,12 +2,14 @@
 #define PAGAMENTO_H
 
 #include <string>
+#include "../../Domains/Headers/CodigoPagamento.h"
 #include "../../Domains/Headers/Data.h"
 #include "../../Domains/Headers/Estado.h"
 #include "../../Domains/Headers/Percentual.h"
 
 class Pagamento {
 private:
+    CodigoPagamento codigo;
     Data data;
     Estado estado;
     Percentual percentual;
@@ -15,10 +17,12 @@ private:
 public:
     Pagamento(std::string data, std::string estado, int percentual);
 
+    void setCodigo(std::string codigo)
     void setData(std::string data);
     void setEstado(std::string estado);
     void setPercentual(int percentual);
 
+    std::string getCodigo();
     std::string getData();
     std::string getEstado();
     int getPercentual();
