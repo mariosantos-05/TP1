cad4e9435ed133ea1b0283b3e508180f00e6efb8
Atila <atila108305@gmail.com>
2024-07-29 19:23:07 -0300

    Interface de Serviço de COnta

---
+++ Interfaces/Int_MSC.h
@@ -0,0 +1,60 @@
+#ifndef INT_MSC_INCLUDE
+#define INT_MSC_INCLUDE
+
+
+#include "TP1\Interfaces\Int_MAC.h"
+#include <stdexcept>
+
+using namespace std;
+
+/**
+ * @class Int_MSC
+ * @brief Interface para prestacao de servicos de conta.
+ * 
+ * Esta classe fornece uma interface abstrata para realizar operações CRUD em uma conta.
+ */
+
+
+class ISConta {
+public:
+
+    /**
+     * @brief Le os atributos de uma conta.
+     * @param CPF* O CPF da conta a ser acessada.
+     * @return A entidade conta, que contem as informaçoes da mesma.
+     */
+
+    virtual Conta ler(CPF) = 0;           
+
+    /**
+     * @brief Atualiza algum dado (senha ou nome) da conta.
+     * @param CPF* O CPF da conta a ser acessada.
+     */
+
+    virtual void atualizar(CPF) = 0;
+
+    /**
+     * @brief Exclui uma conta.
+     * @param CPF* O CPF da conta a ser acessada.
+     */
+
+    virtual void deletar(CPF*) = 0;    
+
+    /**
+     * @brief Liga o MAC ao MSC.
+     * @param CPF* O CPF da conta a ser acessada.
+     */                    
+
+    virtual void setCntrISConta(ISConta*) = 0;    
+
+    /**
+     * @brief Destrutor virtual.
+     */
+  
+    virtual ~ISConta(){}                                     
+};
+
+
+
+
+#endif
