f9ca26efb0184561f4500ea483f803f662eedfdd
At1l4 <atila108305@gmail.com>
2024-07-30 14:45:11 -0300

    Correcoes o metodo criar da Int_MSC

--- Interfaces/Int_MSC.h
+++ Interfaces/Int_MSC.h
@@ -2,7 +2,7 @@
 #define INT_MSC_INCLUDE
 
 
-#include "TP1\Interfaces\Int_MAC.h"
+#include "TP1/Interfaces/Int_MAC.h"
 #include <stdexcept>
 
 using namespace std;
@@ -19,13 +19,22 @@ class ISConta {
 public:
 
     /**
+     * @brief Cria uma conta dados CPF, senha e nome.
+     * @param Senha A senha da conta a ser criada.
+     * @param Nome O nome da conta a ser criada.
+     * @return Booleano que informa sucesso ou falha na criacao.
+     */
+
+    virtual bool criar(CPF cpf, Senha senha, Name nome) = 0;           
+
+    /**
      * @brief Le os atributos de uma conta.
      * @param CPF* O CPF da conta a ser acessada.
      * @return A entidade conta, que contem as informaçoes da mesma.
      */
 
-    virtual Conta ler(CPF) = 0;           
 
+    virtual Conta ler(CPF) = 0;    
     /**
      * @brief Atualiza algum dado (senha ou nome) da conta.
      * @param CPF* O CPF da conta a ser acessada.
