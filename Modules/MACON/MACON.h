b073c7f35b55e6850538471d5f0d3a949cf92bc6
At1l4 <atila108305@gmail.com>
2024-08-01 13:41:50 -0300

    MACON com ponteiros para os moduls

--- Modules/MACON/MACON.h
+++ Modules/MACON/MACON.h
@@ -16,11 +16,25 @@ public:
     void display();
     void handleInput(int ch);
     void showOptions();
+    void setMAC(IAConta*) ;      
+    void setMAA(IAAutenticacao*) ;      
+    void setMAI(IAInvestimento*) ;      
+
 
 private:
     bool isAuthenticated;
     CPF* cpf;
     IAAutenticacao* autenticador;
+    IAConta *acesso_conta; 
+    IAInvestimento *acesso_investimento; 
+
+
 };
 
+void inline MainScreen::setMAC(IAConta *acesso_conta){
+        this->acesso_conta = acesso_conta;
+}
+void inline MainScreen::setMAI(IAConta *acesso_investimento){
+        this->acesso_investimento = acesso_investimento;
+}
 #endif 
