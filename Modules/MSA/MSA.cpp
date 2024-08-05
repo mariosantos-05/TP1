cc7343b54ee1324f499d1c15cdae5c4e20886823
himeakLucas <lucasadeodato.oli@gmail.com>
2024-08-05 14:20:45 -0300

    ???? 2

--- Modules/MSA/MSA.cpp
+++ Modules/MSA/MSA.cpp
@@ -1,6 +1,7 @@
 #include "MSA.h"
 #include <iostream>
 
+/*
 bool AutenticacaoService::autenticar(CPF& cpf, Senha& senha) {
     try {
         Comando_Verificar_Conta cmd(cpf.getCPF(), senha.getSenha());
@@ -10,5 +11,25 @@ bool AutenticacaoService::autenticar(CPF& cpf, Senha& senha) {
     } catch (EErroPersistencia& e) {
         std::cerr << "Erro ao autenticar: " << e.what() << std::endl;
         return false;
+    }
+}
+*/
+
+
+
+
+bool AutenticacaoService::autenticar(CPF& cpf, Senha& senha) {
+    try {
+
+    TestComandoSQL testAuth("");
+    bool authResult = testAuth.autenticar("036.403.621-44", "132465");
+    cout << "Authentication result: " << (authResult ? "Success" : "Failure") << endl;
+    return authResult;
+
+    } catch (EErroPersistencia& e) {
+
+        std::cerr << "Erro ao autenticar: " << e.what() << std::endl;
+        return false;
+
     }
 }
